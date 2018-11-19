b1675a55cba0bb0020e91b8e21338bf3dfd5dd42
D'andre Gregory <dandregregory@symbeint.com>
2018-11-18 19:05:51 -0600

    Added sp customer rating system. Added email and phone to sp profile. Added Vehicle Type constant

--- __test__/service_provider.test.js
+++ __test__/service_provider.test.js
@@ -5,4 +5,5 @@ test('ServiceProvider', () => {
     let sp = new ServiceProvider()
 
     expect(sp).toBeInstanceOf(ServiceProvider)
+    expect(sp.serviceRating()).toEqual(0)
 })
--- src/service_provider.js
+++ src/service_provider.js
@@ -1,8 +1,18 @@
 class ServiceProvider {
+    static VEHICLE_TYPE = {
+        SV: "SERVICE VEHICLE",
+        TR: "TRUCK",
+        WR: "WRECKER",
+        RB: "ROLLBACK",
+    }
+
     constructor(sp) {
         const {
             accountType,
             address,
+            active,
+            email,
+            phone,
             city,
             state,
             zip,
@@ -13,11 +23,18 @@ class ServiceProvider {
             lastName,
             maxServiceArea,
             vehicleType,
+            servicePoints,
+            serviceCallCount,
             createdAt,
             updatedAt,
         } = sp || {}
 
         this.accountType = accountType || undefined
+        this.active = active || false
+        this.email = email || undefined
+        this.phone = phone || undefined
+        this.servicePoints = servicePoints || 0
+        this.serviceCallCount = serviceCallCount || 0
         this.address = address || undefined
         this.city = city || undefined
         this.state = state || undefined
@@ -31,6 +48,13 @@ class ServiceProvider {
         this.vehicleType = vehicleType || undefined
         this.createdAt = createdAt || undefined
         this.updatedAt = updatedAt || undefined
+    }
+
+    serviceRating() {
+        if (this.serviceCallCount === 0) {
+            return 0
+        }
+        return this.servicePoints / this.serviceCallCount
     }
 }
 
