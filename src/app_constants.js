d2a1bf74af4f3ea2706c14e5f98220259aee39d9
D'andre Gregory <dandregregory@symbeint.com>
2018-12-26 12:10:23 -0600

    Added vehicle makes. Added the state names and abbrevations.

--- src/app_constants.js
+++ src/app_constants.js
@@ -1,49 +1,85 @@
-const tow = { item: "EEMERG TOW", cost: 20, qty: 1 };
-const flat = { item: "EEMERG TIRE", cost: 15, qty: 1 };
-const lockout = { item: "EEMERG LOCKOUT", cost: 15, qty: 1 };
-const gas = { item: "EEMERG GAS", cost: 15, qty: 1 };
-const battery = { item: "EEMERG DEADBAT", cost: 15, qty: 1 };
+import Cars from "./cars"
+import States from "./states"
 
-const ptow = { item: "PROVIDER TOW", cost: 20, qty: 1 };
-const pflat = { item: "PROVIDER TIRE", cost: 5, qty: 1 };
-const plockout = { item: "PROVIDER LOCKOUT", cost: 5, qty: 1 };
-const pgas = { item: "PROVIDER GAS", cost: 5, qty: 1 };
-const pdeadbat = { item: "PROVIDER DEADBAT", cost: 5, qty: 1 };
+const tow = { item: "EEMERG TOW", cost: 20, qty: 1 }
+const flat = { item: "EEMERG TIRE", cost: 15, qty: 1 }
+const lockout = { item: "EEMERG LOCKOUT", cost: 15, qty: 1 }
+const gas = { item: "EEMERG GAS", cost: 15, qty: 1 }
+const battery = { item: "EEMERG DEADBAT", cost: 15, qty: 1 }
+
+const ptow = { item: "PROVIDER TOW", cost: 20, qty: 1 }
+const pflat = { item: "PROVIDER TIRE", cost: 5, qty: 1 }
+const plockout = { item: "PROVIDER LOCKOUT", cost: 5, qty: 1 }
+const pgas = { item: "PROVIDER GAS", cost: 5, qty: 1 }
+const pdeadbat = { item: "PROVIDER DEADBAT", cost: 5, qty: 1 }
 
 const BASE_COST_EEMERG = {
     TOW: tow,
     LOCKOUT: lockout,
     FLAT: flat,
     GAS: gas,
-    DEADBAT: battery
-};
+    DEADBAT: battery,
+}
 
 const BASE_COST_PROVIDER = {
     TOW: ptow,
     LOCKOUT: plockout,
     FLAT: pflat,
     GAS: pgas,
-    DEADBAT: pdeadbat
-};
+    DEADBAT: pdeadbat,
+}
 
 const APPCON = {
     BASE_COST_EEMERG: BASE_COST_EEMERG,
-    BASE_COST_PROVIDER: BASE_COST_PROVIDER
-};
+    BASE_COST_PROVIDER: BASE_COST_PROVIDER,
+}
 
-const BIDSLOTS = 2;
+
+
+const BIDSLOTS = 2
 
 const ERT = [
     { text: "1/2Hr to 1Hr", value: "1/2Hr to 1Hr" },
     { text: "1Hr to 2Hr", value: "1Hr to 2Hr" },
     { text: "2Hr to 3Hr", value: "2Hr to 3Hr" },
-    { text: "Unknown", value: "Unknown" }
-];
+    { text: "Unknown", value: "Unknown" },
+]
 const BIDDURATION = [
     { text: "5 MINUTES", value: 6 },
     { text: "10 MINUTES", value: 11 },
-    { text: "15 MINUTES", value: 16 }
-];
+    { text: "15 MINUTES", value: 16 },
+]
+
+const SERVICE_AREA = [
+    { value: 10, text: "10 Mile Radius" },
+    { value: 20, text: "20 Mile Radius" },
+    { value: 30, text: "30 Mile Radius" },
+    { value: 50, text: "50 Mile Radius" },
+    { value: 65, text: "65 Mile Radius" },
+]
+
+const VEHICLE_TYPE = [
+    { value: "", text: "None" },
+    { value: "SV", text: "Service Vehicle" },
+    { value: "TR", text: "Truck" },
+    { value: "WR", text: "Wrecker" },
+    { value: "RB", text: "Rollback" },
+]
 
 
-export {BASE_COST_EEMERG,BASE_COST_PROVIDER,APPCON,BIDDURATION,BIDSLOTS,ERT}
+const US_STATES =  new States().getStatesList();
+
+const VEHICLE_MAKES = new Cars().getCarMakes();
+
+export {
+    BASE_COST_EEMERG,
+    BASE_COST_PROVIDER,
+    APPCON,
+    BIDDURATION,
+    BIDSLOTS,
+    ERT,
+    SERVICE_AREA,
+    VEHICLE_TYPE,
+    VEHICLE_MAKES,
+    US_STATES
+}
